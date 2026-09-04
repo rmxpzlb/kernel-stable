eeb88da5ed0b8871178f550429e482a06b40a4ba
Frank Zhang <rmxpzlb@gmail.com>
2026-09-04 15:54:30 +0800

    rust: rcpufreq_dt: add module alias
    Add the missing `platform:cpufreq-dt` module alias so that the driver
    can be automatically loaded when the platform device is registered.
    
    Fixes: 06149d8f2216 ("cpufreq: Add Rust-based cpufreq-dt driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Frank Zhang <rmxpzlb@gmail.com>

--- drivers/cpufreq/rcpufreq_dt.rs
+++ drivers/cpufreq/rcpufreq_dt.rs
@@ -219,4 +219,5 @@ module_platform_driver! {
     authors: ["Viresh Kumar <viresh.kumar@linaro.org>"],
     description: "Generic CPUFreq DT driver",
     license: "GPL v2",
+    alias: ["platform:cpufreq-dt"],
 }
